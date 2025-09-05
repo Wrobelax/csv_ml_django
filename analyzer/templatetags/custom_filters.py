b6b82bd2b34f31350cb37a45d89b5361c1aef819
Adrian <awrobel.wr@gmail.com>
2025-09-05 14:23:44 +0200

    Project cleaning.

--- analyzer/templatetags/custom_filters.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def format_model(value) -> str:
-    if not value:
-        return value
-    return str(value).replace("_", " ").title()
